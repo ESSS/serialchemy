287282601712b8aed1a1bde2540c25a635e1489a
icandeia <icandeia@esss.co>
2019-03-29 08:58:18 -0300

    Adding session to NestedModelListField load.
    PWDA-5052

--- src/serialchemy/nested_fields.py
+++ src/serialchemy/nested_fields.py
@@ -81,7 +81,7 @@ class NestedModelListField(SessionBasedField):
                 models.append(updated_model)
             else:
                 # No primary key, just create a new model entity
-                model = self.serializer.load(item)
+                model = self.serializer.load(item, session=session)
                 models.append(model)
         return models
 
