9d746ed867552c361cca618ea0c98b522a077b84
Lucas Vinicius Amaral de Oliveira <loliveira@esss.co>
2023-11-13 13:53:22 -0300

    Update supported python versions

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.rst", encoding="UTF-8") as readme_file:
 with open("CHANGELOG.rst", encoding="UTF-8") as changelog_file:
     history = changelog_file.read()
 
-requirements = ["sqlalchemy>=1.3,<2.0"]
+requirements = ["sqlalchemy>=1.4,<2.0"]
 extras_require = {
     "docs": ["sphinx >= 1.4", "sphinx_rtd_theme", "sphinx-autodoc-typehints", "typing_extensions"],
     "testing": [
@@ -32,11 +32,11 @@ setup(
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     description="Serializers for SQLAlchemy models.",
     extras_require=extras_require,
@@ -44,7 +44,7 @@ setup(
     license="MIT license",
     long_description=readme + "\n\n" + history,
     include_package_data=True,
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     keywords="serialchemy",
     data_files=[("", ["LICENSE"])],
     name="serialchemy",
